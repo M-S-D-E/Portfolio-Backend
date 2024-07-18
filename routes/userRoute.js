cc6ddb2903b0d7af2938b04e1da78fd8c5634932
Camey Fiifi Danchi <capochinolive100@gmail.com>
2024-07-18 08:54:06 +0000

    testing

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ export const userRouter= Router();
 
 userRouter.get('/users/getUser', getUsers)
 userRouter.post('/users/auth/signup',signup);
-userRouter.get('/users/:userName', getUser),
+userRouter.get('/users/auth/:userName', getUser),
 
 userRouter.post('/users/auth/login',login)
 userRouter.post("/users/auth/logout", logout);
